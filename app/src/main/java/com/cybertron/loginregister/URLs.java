04056c14bd2db0f347e554d77f3cc9c550f24745
Aditya <iamadi2001@gmail.com>
2021-10-06 18:52:45 +0530

    Added online support

--- app/src/main/java/com/cybertron/loginregister/URLs.java
+++ app/src/main/java/com/cybertron/loginregister/URLs.java
@@ -2,7 +2,7 @@ package com.cybertron.loginregister;
 
 public class URLs {
 
-    private static final String ROOT_URL = "http://192.168.0.100/rest/Api.php?apicall=";
+    private static final String ROOT_URL = "https://mydbapplication.000webhostapp.com/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN= ROOT_URL + "login";
