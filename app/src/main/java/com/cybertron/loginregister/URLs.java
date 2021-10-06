25c8c443598d327e3b4ab6ed84de6a3a1d841f9b
Aditya <iamadi2001@gmail.com>
2021-10-06 18:03:42 +0530

    Login&RegisterUser

---
+++ app/src/main/java/com/cybertron/loginregister/URLs.java
@@ -0,0 +1,10 @@
+package com.cybertron.loginregister;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://192.168.0.100/rest/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
